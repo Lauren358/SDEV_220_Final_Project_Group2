3b1c92c26488d56aea2261c16eb7ba545890af2d
Oceane229 <oatchikpa@ivytech.edu>
2025-12-21 22:57:08 -0500

    Added user autentication

--- foodbank/forms.py
+++ foodbank/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from .models import Inventory
+from .models import Inventory, Take
 
 class DonoForm(forms.ModelForm):
     class Meta:
         model = Inventory
         fields = {'food_name', 'food_total'}
 
-class TakeForm(forms.modelForm):
+class TakeForm(forms.ModelForm):
     class Meta:
         model = Take
         fields = ['name', 'food_name','food_total']
